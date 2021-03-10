6e9a2f07021bfa72aa28b9e7fb9d3ddb01f69974
office-diet <66167894+office-diet@users.noreply.github.com>
2021-03-11 08:08:21 +0900

    DailyUpdate

--- tweet.js
+++ tweet.js
@@ -176,6 +176,7 @@ window.addEventListener("load", function(){
     ["2021/03/09", "https://twitter.com/i/events/1369441781927010304", "江東区塩浜⇒八丁堀⇒皇居⇒日比谷公園⇒新橋", "17.22km", "https://pbs.twimg.com/media/EwE_halWYAUOtIs?format=jpg&name=small"], 
     ["2021/03/09", "https://twitter.com/i/events/1369445971109638144", "築地⇒月島⇒豊洲⇒お台場", "28.33km", "https://pbs.twimg.com/media/EwFClk2WEAUQ8Qu?format=jpg&name=small"], 
     ["2021/03/10", "https://twitter.com/i/events/1369449593440722947", "江東区塩浜⇒夢の島マリーナ⇒新木場緑道公園", "10.22km", "https://pbs.twimg.com/media/EwEfEX0WYAE-HOD?format=jpg&name=small"], 
+    ["2021/03/11", "https://twitter.com/i/events/1369785244275023874", "江東区塩浜⇒砂町銀座商店街⇒南砂", "9.45km", "https://pbs.twimg.com/media/EwJpXodUYAYcZhn?format=jpg&name=small"], 
   ];
   
   const formSearch = document.getElementById("search-form");
