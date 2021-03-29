48db9e8e0975f8ad544cd967420f4b7089dfeec8
office-diet <66167894+office-diet@users.noreply.github.com>
2021-03-29 10:06:57 +0900

    Daily Update

--- tweet.js
+++ tweet.js
@@ -196,6 +196,9 @@ window.addEventListener("load", function(){
     ["2021/03/24", "https://twitter.com/i/events/1374863632253612034", "江東区塩浜⇒東京スカイツリー⇒錦糸町⇒東陽町", "--km", "https://pbs.twimg.com/media/ExNkHx1VEAMmqEh?format=jpg&name=small"], 
     ["2021/03/25", "https://twitter.com/i/events/1374866061472923648", "江東区塩浜⇒門前仲町周辺", "8.33km", "https://pbs.twimg.com/media/ExRWC2vU8AELgHb?format=jpg&name=small"], 
     ["2021/03/26", "https://twitter.com/i/events/1375219740591845382", "江東区塩浜⇒豊洲⇒東雲⇒辰巳", "11.17km", "https://pbs.twimg.com/media/ExWmNjrVcAMlz6j?format=jpg&name=small"], 
+    ["2021/03/26", "https://twitter.com/i/events/1376327983946854400", "江東区塩浜⇒潮見⇒夢の島マリーナ⇒新木場緑道公園", "--km", "https://pbs.twimg.com/media/ExZ7LdqVIAEyY9U?format=jpg&name=small"], 
+    ["2021/03/27", "https://twitter.com/i/events/1376330056587374596", "江東区塩浜⇒鎌ヶ谷⇒船橋⇒印西⇒白井", "122.52km", "https://pbs.twimg.com/media/Exm70cNVoAER1L9?format=jpg&name=small"], 
+    ["2021/03/29", "https://twitter.com/i/events/1376338009486807045", "江東区塩浜⇒豊洲", "7.76km", "https://pbs.twimg.com/media/ExmDg-3VcAUmYJc?format=jpg&name=small"], 
   ];
   
   const formSearch = document.getElementById("search-form");
