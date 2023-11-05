bcd12f2b639b9a1a22d22d405dc2fe202cdf8cc0
office-diet <66167894+office-diet@users.noreply.github.com>
2023-11-05 10:28:35 +0900

    add new tweet

--- tweet.js
+++ tweet.js
@@ -431,6 +431,7 @@ window.addEventListener("load", function(){
     ["2023/06/14", "https://office-diet.github.io/tweets/20230614.html", "東陽町⇒西大島⇒笹塚⇒明大前⇒千歳烏山", "--km", "https://pbs.twimg.com/media/Fyihus0aYAA79p-?format=jpg&name=small"], 
     ["2023/06/17", "https://office-diet.github.io/tweets/20230617.html", "東陽町⇒浦安⇒妙典⇒西船橋⇒南船橋", "--km", "https://pbs.twimg.com/media/FyyHzkDaUAA7oNk?format=jpg&name=small"], 
     ["2023/11/03", "https://office-diet.github.io/tweets/20231103.html", "東陽町⇒新長島川親水公園⇒浦安公園⇒行徳", "--km", "https://pbs.twimg.com/media/F99u6KlaQAAH6Gm?format=jpg&name=small"], 
+    ["2023/11/04", "https://office-diet.github.io/tweets/20231104.html", "塩浜⇒潮見さざなみ公園⇒辰巳の森海浜公園", "--km", "https://pbs.twimg.com/media/F-FykxkbIAAGSCG?format=jpg&name=small"], 
   ];
   
   const formSearch = document.getElementById("search-form");
